8edf05401efd1b68ab01e7f8349fea664a131bc6
Lord-Mahdi1383 <M.Esmaeely.83@gmail.com>
2026-02-08 12:00:19 +0330

    added collision function

---
+++ snake_game_pygame.py
@@ -0,0 +1,41 @@
+import pygame
+import random
+import numpy as np
+
+# constants
+SPEED = 30
+BLOCK_SIZE = 30
+
+RIGHT = 1 
+LEFT = 2
+UP = 3
+DOWN = 4
+
+
+class snake():
+    def __init__(self, w=640, h=480):
+        pass
+
+
+    def reset(self):
+        pass
+
+
+    def place_food(self):
+        pass
+
+
+    def play_step(self, action):
+        pass
+
+
+    def collision(self, pt=None):
+        pass
+
+
+    def update_ui(self):
+        self.display.fill((0,0,0)):
+
+    
+    def move(self, action):
+        pass
